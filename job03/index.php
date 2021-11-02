241dabba747bd9e3c92c988c08ce4289477507cf
marc-keil <91876158+marc-keil@users.noreply.github.com>
2021-11-02 19:47:44 +0100

    Add files via upload

---
+++ job03/index.php
@@ -0,0 +1,40 @@
+<html>
+    <?php
+
+$myBooleen = true; # booleen
+$entiers = 3; # int
+$chaine = 'chainedecaracte'; # string
+$virgule = 1.233; # float
+?>
+<header>
+    <table border=1>
+      <tr>
+           <td>Nom</td>
+           <td>Type</td>
+           <td>Valeur</td>
+      </tr>
+</header>
+<body>
+   <tr>
+      <td>Chaine De Caractère</td>
+      <td>String</td>
+      <td>chaine</td>
+   </tr>
+   <tr>
+      <td>Nombres Entiers</td>
+      <td>Int</td>
+      <td>3</td>
+   </tr>
+   <tr>
+      <td>Nombres Décimaux</td>
+      <td>Float</td>
+      <td>1.233</td>
+   </tr>
+   <tr>
+      <td>Booléens</td>
+      <td>Bool</td>
+      <td>1</td>
+   </tr>
+</body>
+</table>
+<body><html>
