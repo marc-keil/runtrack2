ebc46dceb8adab527a505ec2db22f13fa5a69ad5
marc-keil <91876158+marc-keil@users.noreply.github.com>
2021-11-05 09:59:43 +0100

    Add files via upload

---
+++ jour03/job06/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$str = "Les choses que l'on possede finissent par nous posseder.";
+for ($i=0; isset($str[$i]) == true; $i++); 
+for ($i = $i = -1; isset($str[$i]); $i--) {
+echo $str[$i];
+    
+}
