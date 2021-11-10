70fe8e1551ca865899a32b652beb0af34751adeb
marc-keil <91876158+marc-keil@users.noreply.github.com>
2021-11-10 15:16:59 +0100

    Add files via upload

--- jour07/job04/index.php
+++ jour07/job04/index.php
@@ -1,22 +1,38 @@
-<html>
-<form action="index.php" method="post">
-        <label for="prenom">Votre Prénom:</label>
-    <br/>
-        <input type="text" name="prenom" id="prenom">
-        <input type="submit" name="Connexion" value="Connexion">
-
-    </form><br/>
-</html>    
 <?php
 
 if(isset($_POST['prenom']))
 {
-    setcookie('prenom',$_POST['prenom']);
+    setcookie('prenom',$_POST['prenom'], time()+3600);
     header('Location: index.php');
 }
-
-
-
-
+if(isset($_POST['deco'])){
+    unset($_COOKIE['prenom']);
+    // header('Location: index.php');
+}
 
 ?>
+
+<html>
+    <?php 
+
+if(!isset($_COOKIE['prenom'])) {
+echo "
+<form action='index.php' method='post'>
+<label for='prenom'>Votre Prénom:</label><BR>
+<br/>
+<input type='text' name='prenom' id='prenom'><br><br>
+<input type='submit' name='Connexion' value='Connexion'>
+</form><br/>
+";
+}
+else {
+    echo 
+    "Bonjour ".$_COOKIE['prenom'].
+    " !
+    <form action='index.php' method='post'<br><br>  
+    <input type='submit' name='deco' value='Déconnexion'>
+    </form><br/>
+    ";
+}
+    ?>
+</html>    
